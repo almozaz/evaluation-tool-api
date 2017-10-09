a0b747641ca81c333dcacd49e8c685516138b66c
almozaz <alacruzmozaz@gmail.com>
2017-10-09 16:53:58 +0200

    Create students service

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const classes = require('./classes/classes.service.js');
+const students = require('./students/students.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(classes);
+  app.configure(students);
 };
