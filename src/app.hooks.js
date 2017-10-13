ea4d630587060c89c4f2b9ce6a41b7bb4afefcdc
almozaz <alacruzmozaz@gmail.com>
2017-10-13 11:54:55 +0200

    Add authentication and restrict to authenticated hook for all actions

--- src/app.hooks.js
+++ src/app.hooks.js
@@ -1,9 +1,17 @@
 // Application hooks that run for every service
 const logger = require('./hooks/logger');
 
+const { authenticate } = require('feathers-authentication').hooks;
+const { restrictToAuthenticated } = require('feathers-authentication-hooks');
+
+const restrict = [
+  authenticate('jwt'),
+  restrictToAuthenticated(),
+];
+
 module.exports = {
   before: {
-    all: [],
+    all: [...restrict],
     find: [],
     get: [],
     create: [],
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 const classes = require('./classes/classes.service.js');
 const students = require('./students/students.service.js');
 const evaluations = require('./evaluations/evaluations.service.js');
+const users = require('./users/users.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(classes);
   app.configure(students);
   app.configure(evaluations);
+  app.configure(users);
 };
